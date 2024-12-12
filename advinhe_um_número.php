a164c5a5ee6e7b390de7e0f1376e78ff143b705a
brenda14k <brendam.ifpr2024@gmail.com>
2024-12-12 09:11:13 -0300

    Add files via upload

--- advinhe_um_número.php
+++ advinhe_um_número.php
@@ -1,35 +1,53 @@
-<?php
-ranking();
+    <?php
 
-$sorteado = rand(1,100);
-$tentativas = 0;
-do {
+
+    $sorteado = rand(1,100);
+    $tentativas = 0;
     do {
-    
-        $chute = (int)readline ("Informe um número: ");
-        } while(!is_numeric($chute));
-        $tentativas++;
+        do {
         
-        if ($chute == $sorteado) {
-            print("Parabéns você acertou! \n");
+            $chute = (int)readline ("Informe um número: ");
+            } while(!is_numeric($chute));
+            $tentativas++;
             
-        print("Foram necesárias {$tentativas}! \n");
-        }else{
-            print("Ahh,você errou! Não era o número $chute.\n");
-            if ($chute > $sorteado){
-                print("O chute foi muito alto!\n");
+            if ($chute == $sorteado) {
+                print("Parabéns você acertou! \n");
+                
+            print("Foram necesárias {$tentativas}! \n");
+            
+            $nome = readline("Informe seu nome para o ranking: ");
+            ranking($nome,$tentativas);
+            
             }else{
-                print("O chute foi muito baixo!\n");
+                print("Ahh,você errou! Não era o número $chute.\n");
+                if ($chute > $sorteado){
+                    print("O chute foi muito alto!\n");
+                }else{
+                    print("O chute foi muito baixo!\n");
+                }
             }
-        }
-        
-} while ($chute != $sorteado);
+            
+    } while ($chute != $sorteado);
 
-function ranking (){
-    $nome = 'Brenda';
-    $tentativas = 6;
-    $ranking = [];
-    array_push($ranking,"teste");
-    array_push($ranking,"teste");
-print_r($ranking);
-}
+    function ranking ($nome, $quantidade_jogadas){
+        
+        // lê os dados que já estão no arquivo
+        $json = file_get_contents("ranking.json");
+        
+        // transforma ou decodifica 
+        $ranking = json_decode($json,true);
+        
+        $jogada = [
+            "nome" => $nome,
+            "jogadas" => $quantidade_jogadas
+        ];
+        
+    $ranking[] = $jogada;
+    
+    //transforma os dados para o formato json
+    
+    $json = json_encode($ranking,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    //salva um dado em um arquivo
+    file_put_contents("ranking.json",$json);
+
+    }
