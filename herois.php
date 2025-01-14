3e8f6f5c18036b35a879f3f587fa150bbbfaedc7
brenda14k <brendam.ifpr2024@gmail.com>
2025-01-14 11:31:59 -0300

    Add files via upload

---
+++ herois.php
@@ -0,0 +1,13 @@
+<?php
+print ("***TOP HEROIS DO MUNDO***\n");
+print"\n";
+$herois = array('thor','capitao america','homem de ferro','viuva negra','menino formiga',);
+
+
+
+for ($i=0; $i < 5; $i++) { 
+    print $i+1 . "°  $herois[$i] \n";
+
+
+        
+    }
