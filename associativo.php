a1b3240660c273f9d49569744e843a9219b665be
brenda14k <brendam.ifpr2024@gmail.com>
2025-01-14 11:31:15 -0300

    Add files via upload

---
+++ associativo.php
@@ -0,0 +1,13 @@
+<?php
+    
+$fruta = array(
+    "cor" => "vermelha",
+    "sabor" => "doce",
+    "formato" => "redonda",
+    "nome" => "maça"
+);
+foreach ($fruta as $chave => $caracteristica) {
+    
+    print "$chave-$caracteristica \n";
+    
+}
